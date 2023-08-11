153cd74a3e53ee8a95792abb143271676681b875
Austin Papritz <austin@papritz.dev>
2023-08-11 10:54:24 -0700

    updated DB seeder with flavors and treats

--- PierresTasties/Models/DataInitializer.cs
+++ PierresTasties/Models/DataInitializer.cs
@@ -4,31 +4,59 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-
-namespace PierresTasties.Models;
-
-public class DataInitializer
+namespace PierresTasties.Models
 {
-    public static void InitializeData(WebApplication app)
+    public class DataInitializer
     {
-        using (var scope = app.Services.CreateScope())
+        public static void InitializeData(WebApplication app)
         {
-            var context = scope.ServiceProvider.GetRequiredService<PierresTastiesContext>();
-            context.Database.Migrate();
-
-            // If there's already stuff in db, don't run.
-            if (context.ExampleModels.Any())
+            using (var scope = app.Services.CreateScope())
             {
-                return;
+                var context = scope.ServiceProvider.GetRequiredService<PierresTastiesContext>();
+                context.Database.Migrate();
+
+                // If there's already stuff in the database, don't run.
+                if (context.Flavors.Any())
+                {
+                    return;
+                }
+
+                // Define Flavors
+                var flavors = new List<Flavor>
+                {
+                    new Flavor { Name = "Sweet" },
+                    new Flavor { Name = "Savory" },
+                    new Flavor { Name = "Creamy" },
+                    new Flavor { Name = "Spicy" }
+                };
+
+                // Save Flavors to the database
+                context.Flavors.AddRange(flavors);
+                context.SaveChanges();
+
+                // Define Treats with combinations of flavors
+                var treatNames = new List<string>
+                {
+                    "Sweet Raspberry Tart", "Sweet Lemon Cookies", "Sweet Almond Cake", "Sweet Chocolate Muffins",
+                    "Savory Cheese Croissant", "Savory Olive Bread", "Savory Tomato Quiche", "Savory Bacon Scones",
+                    "Creamy Vanilla Eclairs", "Creamy Coffee Tiramisu", "Creamy Strawberry Cheesecake", "Creamy Pistachio Mousse",
+                    "Spicy Ginger Cookies", "Spicy Chili Brownies", "Spicy Cinnamon Rolls", "Spicy Cayenne Chocolate Cake"
+                };
+
+                // Create FlavorTreat relationships
+                for (int i = 0; i < flavors.Count; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        var treat = new Treat { Name = treatNames[i * 4 + j] };
+                        var flavorTreat = new FlavorTreat { Flavor = flavors[i], Treat = treat };
+                        context.FlavorTreats.Add(flavorTreat);
+                    }
+                }
+
+                // Save changes to the database
+                context.SaveChanges();
             }
-
-            var exampleModel = new ExampleModel[]
-            {
-                new ExampleModel { Name = "Example Name"}
-            };
-
-            context.ExampleModels.AddRange(exampleModel);
-            context.SaveChanges();
         }
     }
 }
