482718405eead5b97034fcaaff22b16126a9dc36
Austin Papritz <austin@papritz.dev>
2023-08-11 19:21:17 -0700

    user starts with three VoteTokens

--- PierresTasties/Models/ApplicationUser.cs
+++ PierresTasties/Models/ApplicationUser.cs
@@ -4,6 +4,10 @@ namespace PierresTasties.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    public ApplicationUser()
+    {
+        VoteTokens = 3;
+    }
     public int VoteTokens { get; set; }
     public ICollection<Vote> Votes { get; set; }
 }
